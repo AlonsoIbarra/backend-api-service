7f75f08b6d008e346fc03cdd11c4be862b2d4934
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2023-05-26 11:41:20 -0600

    Fix bcrypt import

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-const { compareSync, hashSync, genSaltSync } = require('bcryptjs');
+const { compareSync, hashSync, genSaltSync } = require('bcrypt');
 
 const UserSchema = new Schema({
     name: {type: String, required: true},
