1aa299e0e1fa91cf36f176521ce97e53c3b071de
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2023-05-26 19:27:26 -0600

    Create JWT generateToken function

---
+++ src/helpers/jwt.helper.js
@@ -0,0 +1,10 @@
+const { sign } = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config")
+
+module.exports.generateToken = function(user){
+    return sign(
+        {user},
+        JWT_SECRET,
+        { expiresIn: "30d" }
+    );
+};
