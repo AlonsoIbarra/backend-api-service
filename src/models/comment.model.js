5ff7c73f4bdb509115db846b77c77912e3ceb278
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2023-05-27 22:49:37 -0600

    Update CommentSchema, set comment as required

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const { Schema} = mongoose;
 
 const CommentSchema = new Schema({
-    comment: {type: String},
+    comment: {
+        type: String,
+        required: true
+    },
     author: {
         type: Schema.Types.ObjectId,
         ref: "user",
