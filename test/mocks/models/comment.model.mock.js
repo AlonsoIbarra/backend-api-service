a33d9f9e1101e59fb1d73f29dd1e47a8434116d2
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2023-05-28 16:55:06 -0600

    Update Model mocks

--- test/mocks/models/comment.model.mock.js
+++ test/mocks/models/comment.model.mock.js
@@ -1,36 +1,21 @@
 module.exports = {
     comment: {
         "_id": "6472d2a6b19d21f0e8e7b57d",
-        "comment": "Awsome idea comment...",
-        "author": {
-            "_id": "6471548c7e475824456f8ee1",
-            "name": "alonso",
-            "username": "alonso",
-            "__v": 0
-        },
+        "comment": "Awsome idea comment.",
+        "author": "6471548c7e475824456f8ee1",
         "__v": 0
     },
     comments: [
         {
             "_id": "6472d2a6b19d21f0e8e7b57d",
-            "comment": "Nice idea buddy.",
-            "author": {
-                "_id": "6471548c7e475824456f8ee1",
-                "name": "alonso",
-                "username": "alonso",
-                "__v": 0
-            },
+            "comment": "Awsome idea comment.",
+            "author": "6471548c7e475824456f8ee1",
             "__v": 0
         },
         {
             "_id": "6472db978200a39222c5b433",
             "comment": "Cool man!",
-            "author": {
-                "_id": "6471548c7e475824456f8ee1",
-                "name": "alonso",
-                "username": "alonso",
-                "__v": 0
-            },
+            "author": "647249df3ea8e12c7f999b5b",
             "__v": 0
         }
     ]
--- test/mocks/models/idea.model.mock.js
+++ test/mocks/models/idea.model.mock.js
@@ -1,86 +1,23 @@
 module.exports = {
     idea: {
-        "_id": "64724b1c3ea8e12c7f999b82",
         "idea": "idea 555",
-        "description": "Awsome idea updated...",
+        "description": "Awsome idea.",
         "upvotes": [true, true],
         "downvotes": [],
-        "author": {
-            "_id": "6471548c7e475824456f8ee1",
-            "name": "alonso",
-            "username": "alonso",
-            "__v": 0
-        },
-        "comments": [
-            {
-                "_id": "6472d2a6b19d21f0e8e7b57d",
-                "comment": "Awsome idea comment..",
-                "author": {
-                    "_id": "6471548c7e475824456f8ee1",
-                    "name": "alonso",
-                    "username": "alonso",
-                    "__v": 0
-                },
-                "__v": 0
-            },
-            {
-                "_id": "6472db978200a39222c5b433",
-                "comment": "Another comment.",
-                "author": {
-                    "_id": "6471548c7e475824456f8ee1",
-                    "name": "alonso",
-                    "username": "alonso",
-                    "__v": 0
-                },
-                "__v": 0
-            }
-        ],
+        "author": "6471548c7e475824456f8ee1",
+        "comments": [],
+        "_id": "64724b1c3ea8e12c7f999b82",
         "__v": 0
     },
     ideas: [
         {
-            "_id": "64724b403ea8e12c7f999b87",
-            "idea": "idea 6",
-            "description": "Awsome idea...",
-            "upvotes": [],
+            "idea": "idea 555",
+            "description": "Awsome idea.",
+            "upvotes": [true, true],
             "downvotes": [],
-            "author": {
-                "_id": "647249df3ea8e12c7f999b5b",
-                "name": "user1",
-                "username": "user1",
-                "__v": 0
-            },
+            "author": "6471548c7e475824456f8ee1",
             "comments": [],
-            "__v": 0
-        },
-        {
-            "_id": "64724b443ea8e12c7f999b8a",
-            "idea": "idea 7",
-            "description": "A gratefull idea.",
-            "upvotes": [],
-            "downvotes": [],
-            "author": {
-                "_id": "647249df3ea8e12c7f999b5b",
-                "name": "user1",
-                "username": "user1",
-                "__v": 0
-            },
-            "comments": [],
-            "__v": 0
-        },
-        {
-            "_id": "64724b503ea8e12c7f999b8d",
-            "idea": "idea 8",
-            "description": "Awsome idea...",
-            "upvotes": [],
-            "downvotes": [],
-            "author": {
-                "_id": "647249e63ea8e12c7f999b5e",
-                "name": "user2",
-                "username": "user2",
-                "__v": 0
-            },
-            "comments": [],
+            "_id": "64724b1c3ea8e12c7f999b82",
             "__v": 0
         }
     ]
