ff6a5793e4b6cb4d96bed8e837a84384bd4e05a1
Saul Alonso Ibarra <saul.ibarra@cimat.mx>
2023-05-28 15:58:10 -0600

    Create model mocks for testing

---
+++ test/mocks/models/user.model.mock.js
@@ -0,0 +1,46 @@
+module.exports = {
+    user: {
+        "_id": "6471548c7e475824456f8ee1",
+        "name": "alonso",
+        "username": "alonso",
+        "password": "secret",
+        "__v": 0
+    },
+    users: [
+        {
+            "_id": "647249df3ea8e12c7f999b5b",
+            "name": "user1",
+            "username": "user1",
+            "password": "secret",
+            "__v": 0
+        },
+        {
+            "_id": "647249e63ea8e12c7f999b5e",
+            "name": "user2",
+            "username": "user2",
+            "password": "secret",
+            "__v": 0
+        },
+        {
+            "_id": "647249ec3ea8e12c7f999b61",
+            "name": "user3",
+            "username": "user3",
+            "password": "secret",
+            "__v": 0
+        },
+        {
+            "_id": "647249f23ea8e12c7f999b64",
+            "name": "user4",
+            "username": "user4",
+            "password": "secret",
+            "__v": 0
+        },
+        {
+            "_id": "647249f83ea8e12c7f999b67",
+            "name": "user5",
+            "username": "user5",
+            "password": "secret",
+            "__v": 0
+        },
+    ]
+};
